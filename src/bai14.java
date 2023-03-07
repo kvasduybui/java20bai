9f2651569e05703ce7bf918a058b9f7918b6e6f0
Kvas Duy Bui <builengocduy300901@gmail.com>
2023-03-07 14:49:38 +0700

    Fix bai14.java

--- src/bai14.java
+++ src/bai14.java
@@ -1,30 +1,25 @@
 import java.util.Scanner;
 
-public class bai14 {
+public class N19DCVT005_OOP_TH2_09 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] mang5So = new int[5];
+        String[] mssv = new String[5];
 
-        System.out.println("Nhap 5 so: ");
         for (int i = 0; i < 5; i++) {
-            System.out.print("mang5So[" + i + "] = ");
-            mang5So[i] = input.nextInt();
+            System.out.print("Nhap ma so sinh vien thu " + i + ": ");
+            mssv[i] = input.nextLine();
         }
+
+        System.out.println();
         
-        String ma4SoCuoi = "";
-        for (int i = 1; i < 5; i++) {
-            ma4SoCuoi += mang5So[i];
-        }
-        String mssv = "00" + mang5So[0] + "L" + ma4SoCuoi;
-        
-        if (mssv.matches("^00[2-5]{1}L[1-9]{4}$")) {
-            System.out.println("Ma so sinh vien hop le");
-        } else {
-            System.out.println("Ma so sinh vien khong hop le");
+        for (int i = 0; i < 5; i++) {
+            if (mssv[i].matches("^B170[1-9]{4}$")) {
+                System.out.println("Ma so sinh vien thu " + i + " hop le");
+            } else {
+                System.out.println("Ma so sinh vien thu " + i + " khong hop le");
+            }
         }
 
         input.close();
     }
 }
-
-
